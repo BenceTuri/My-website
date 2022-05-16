c94a7f95ec94e75b8785bc2d61d99004e4429cfe
BenceTuri <benceturi2@gmail.com>
2022-05-16 17:22:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# My-website
+ My website host and source code(It is basically my first html5 site so plz don't be mad)
---
+++ js/script.js
@@ -0,0 +1,7 @@
+//very very long JavaScript code
+function aboutPage() {
+    window.open("../html/about.html")
+}
+function monkeyLand() {
+    window.open("../html/monkeyland.html")
+}
