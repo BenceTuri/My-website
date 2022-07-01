310615a3b23578b258e6f399bf23191cc9e032ea
BenceTuri <benceturi2@gmail.com>
2022-07-01 19:55:50 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //very very long JavaScript code
 function aboutPage() {
-    window.open("../html/about.html")
+    window.open("../html/about.html", "_self")
 }
 function monkeyLand() {
-    window.open("../html/monkeyland.html")
+    window.open("../html/monkeyland.html", "_self")
 }
